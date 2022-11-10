a06314dd026d9f61c2f4d8682030b68b2a382fce
Luis Barcelos <luisgmbarcelos@gmail.com>
2022-11-10 19:56:50 -0300

    feat: first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'This is MEU MANINHO first API call!'
+
+
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True) 
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn)
