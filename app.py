f09191ed07175e48e4f877313115459fa2f2f3da
Arthur Pereira dos Santos <arthur@pop-os.localdomain>
2022-11-16 23:42:06 -0300

    APIs para ambas estufas

--- app.py
+++ app.py
@@ -1,39 +1,59 @@
 from flask import Flask, request, jsonify
 import json
+
+from estufa_ar import *
+from estufa_solo import *
+from banco_de_dados import *
+
 app = Flask(__name__)
 
+class apiEstufaAr():
+     
+     @app.route("/estufa-ar/add", methods=["POST"])
+     def postEstufaAr():
+          registro = request.get_json()
+          return jsonify(setRegistroEstufaAr(registro))
 
-@app.route('/')
-def hello_world():
-    return 'This is MEU MANINHO first API call!'
+     @app.route("/estufa-ar", methods=["GET"])
+     def getEstufasAr():
+          return jsonify(getRegistrosEstufaAr)
 
-# ----------- POSTs -----------------------
-@app.route('/estufa-ar', methods=["POST"])
-def postEstufaAr():
-     input_json = request.get_json(force=True)
-     with open ("estufar_ar.json", "r+") as fileData:
-        data = json.load(fileData)
-        jsonString = json.dumps(input_json)
-        data.update(jsonString)
-        fileData.write(data)
-     return jsonify(jsonString)
+     @app.route("/estufa-ar/<registro_pk>", methods=["GET"])
+     def getEstufaAr(registro_pk):
+          return jsonify(getRegistroEstufaAr(registro_pk))
 
-@app.route('/estufa-solo', methods=["POST"])
-def postEstufaSolo():
-     input_json = request.get_json(force=True) 
-     dictToReturn = {'text':input_json['text']}
-     return jsonify(dictToReturn)
+     @app.route("/estufa-ar/update", methods=["PUT"])
+     def updateEstufaAr():
+          registro = request.get_json()
+          return jsonify(atualizaRegistroEstufaAr(registro))
+
+     @app.route("/estufa-ar/delete/<registro_pk>", methods=["DELETE"])
+     def deleteEstufaAr(registro_pk):
+          return jsonify(removeRegistroEstufaAr(registro_pk))
+
+class apiEstufaSolo():
+
+     @app.route("/estufa-solo/add", methods=["POST"])
+     def postEstufaSolo():
+          registro = request.get_json()
+          return jsonify(setRegistroEstufaSolo(registro))
+
+     @app.route("/estufa-solo", methods=["GET"])
+     def getEstufasSolo():
+          return jsonify(getRegistrosEstufaSolo)
+
+     @app.route("/estufa-solo/<registro_pk>", methods=["GET"])
+     def getEstufaSolo(registro_pk):
+          return jsonify(getRegistroEstufaSolo(registro_pk))
+
+     @app.route("/estufa-solo/update", methods=["PUT"])
+     def updateEstufaSolo():
+          registro = request.get_json()
+          return jsonify(atualizaRegistroEstufaSolo(registro))
+
+     @app.route("/estufa-solo/delete/<registro_pk>", methods=["DELETE"])
+     def deleteEstufaSolo(registro_pk):
+          return jsonify(removeRegistroEstufaSolo(registro_pk))
 
 
-# ----------- GETs -----------------------
-@app.route('/estufa-ar', methods=["GET"])
-def getEstufaAr():
-     input_json = request.get_json(force=True) 
-     dictToReturn = {'text':input_json['text']}
-     return jsonify(dictToReturn)
 
-@app.route('/estufa-solo', methods=["GET"])
-def getEstufaSolo():
-     input_json = request.get_json(force=True) 
-     dictToReturn = {'text':input_json['text']}
-     return jsonify(dictToReturn)
