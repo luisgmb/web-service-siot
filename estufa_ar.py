dceed1e48edaceecc3c17f758c624bc9fac87d0b
Arthur Santos <arthurperdsantos@gmail.com>
2022-11-24 21:43:54 -0300

    Retirada put e del

--- estufa_ar.py
+++ estufa_ar.py
@@ -34,7 +34,8 @@ def setRegistroEstufaAr(registro: dict = None):
         conn = conecta_banco()
         cursor = conn.cursor()
         humidade_ar = registro.get("humidade_ar", None)
-        direcao = registro.get("direcao", None)
+        direcao_ar = registro.get("direcao", None)
+        print(f'Direcao = {direcao_ar}')
         pressao = registro.get("pressao", None)
         velocidade_do_vento = registro.get("velocidade_do_vento", None)
         temperatura_ar = registro.get("temperatura_ar", None)
@@ -42,7 +43,7 @@ def setRegistroEstufaAr(registro: dict = None):
         data_registro = datetime.now()
         cursor.execute(
             "INSERT INTO estufaAr (humidade_ar, direcao, pressao, velocidade_do_vento, temperatura_ar, luminosidade, data_registro)"
-            f" VALUES ({humidade_ar}, {direcao}, {pressao}, {velocidade_do_vento}, {temperatura_ar}, {luminosidade}, {data_registro})"
+            f" VALUES ({humidade_ar}, '{direcao_ar}', {pressao}, {velocidade_do_vento}, {temperatura_ar}, {luminosidade}, '{data_registro}')"
         )
         conn.commit()
         registro = getRegistroEstufaAr(cursor.lastrowid)
@@ -115,75 +116,3 @@ def getRegistroEstufaAr(registro_pk: int):
         conn.close()
 
     return registro
-
-
-def atualizaRegistroEstufaAr(registro: dict, registro_pk: int):
-    """_summary_
-
-    Args:
-        registro (dict): dados a serem atualizados
-        registro_pk (int): ID do registro a ser atualizado
-
-    Returns:
-        dict: registro atualizado
-    """
-    try:
-        conn = conecta_banco()
-        cursor = conn.cursor()
-
-        if not registro_pk:
-            raise Exception("É necessário informar o id do registro")
-
-        humidade_solo = registro.get("humidade_solo", None)
-        temperatura_solo = registro.get("temperatura_solo", None)
-        data_registro = datetime.now()
-
-        cursor.execute(
-            f"SELECT estufaAr SET humidade_solo = {humidade_solo}, temperatura_solo ={temperatura_solo},"
-            f" data_registro={data_registro} WHERE registro_pk={registro_pk}"
-        )
-
-        conn.commit()
-        registro = getRegistroEstufaAr(registro_pk)
-
-    except Exception as erro:
-        conn.rollback()
-        logger.error(f"Houver um erro ao consultar o registro {registro_pk}, erro: {erro}")
-        registro = {}
-
-    finally:
-        conn.close()
-
-    return registro
-
-
-def removeRegistroEstufaAr(registro_pk: int):
-    """_summary_
-
-    Args:
-        registro_pk (int): ID do registro a ser removido
-
-    Returns:
-        tupla:
-            status (boolean): Status da remoção
-            mensagem (string): Mensagem informando resultado da remoção
-    """
-    try:
-        conn = conecta_banco()
-        conn.execute(f"DELETE from estufaAr WHERE registro_pk={registro_pk}")
-        conn.commit()
-        mensagem = f"Registro removido {registro_pk} com sucesso"
-        logger.info(mensagem)
-        status = True, mensagem
-
-    except Exception as erro:
-        conn.rollback()
-        mensagem = f"Ocorreu um erro ao remover o registro {registro_pk}, erro: {erro}"
-
-        logger.error(mensagem)
-        status = False, mensagem
-
-    finally:
-        conn.close()
-
-    return status
