602ff77014da282df1f566fa4371daa45712a2ab
Luis Barcelos <luisgmbarcelos@gmail.com>
2022-12-08 19:20:48 -0300

    simple commit

--- serial_write.py
+++ serial_write.py
@@ -31,10 +31,18 @@ send = serial.Serial(
 comunicaSolo = bytearray.fromhex('1C 03 01 21 05')
 comunicaAr = bytearray.fromhex('1C 02 01 21 05')
 
-while True:
+send.write(comunicaSolo)
 
- send.write(comunicaSolo)
+print(comunicaSolo)
 
- print(comunicaSolo)
+time.sleep(1.5)
 
- time.sleep(1.5)
+GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+
+data_raw = ser.read(1)
+if data_raw == b"\x1c":
+    data_raw += ser.read_until(b'\x05')
+    print(str(data_raw))
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+    time.sleep(.5)
